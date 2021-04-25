f253909fd0e8c436c8e53e0c98f24a0f13cdb15c
Dilli Babu R <dillir07@gmail.com>
2021-04-25 11:42:05 +0530

    using react now

---
+++ build/precache-manifest.63a01906a0ae2032381e7a3a00dc5157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e6bb97ddd6f0aa5b7761e0461c90fc",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "32959b3edbf12c876407",
+    "url": "/tictactoe/static/css/main.4573bd2c.chunk.css"
+  },
+  {
+    "revision": "d1794602387eb6bf4243",
+    "url": "/tictactoe/static/js/2.52f96d4b.chunk.js"
+  },
+  {
+    "revision": "4c98c6b556b8cc39dae85626e0284631",
+    "url": "/tictactoe/static/js/2.52f96d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32959b3edbf12c876407",
+    "url": "/tictactoe/static/js/main.be2e6a67.chunk.js"
+  },
+  {
+    "revision": "7f372e9ec940eae4f809",
+    "url": "/tictactoe/static/js/runtime-main.3152b602.js"
+  }
+]);
