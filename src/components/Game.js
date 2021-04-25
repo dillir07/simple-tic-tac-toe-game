6796a654977bc4adf37346c2005c8e874c56e42d
Dilli Babu R <dillir07@gmail.com>
2021-04-25 13:40:40 +0530

    using react now

--- src/components/Game.js
+++ src/components/Game.js
@@ -68,7 +68,7 @@ const Game = () => {
 
     const renderMoves = () =>
         history.map((_step, move) => {
-            const destination = move ? `Go to move #${move}` : "Go to Start";
+            const destination = move ? `Go to move #${move}` : "Go to Start or Replay";
             return (
                 <li key={move}>
                     <button onClick={() => jumpTo(move)}>{destination}</button>
